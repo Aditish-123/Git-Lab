d7c18fb29d5f0f8dec4d2e0f8e9eaee880b1607c
Aditish-116 <mandal.sharma2004@gmail.com>
2026-08-14 14:20:44 +0530

    Update App.java

--- src/main/java/1/App.java
+++ src/main/java/1/App.java
@@ -1,10 +1,9 @@
-package 1;
-
-/**
- * Hello world!
- */
-public class App {
+public class SimpleAdd {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("Add two numbers: " + sum);
     }
 }
